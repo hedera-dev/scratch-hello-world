552a29f7078fce58c814905ffb13269e0a8a3550
Brendan Graetz <bguiz@users.noreply.github.com>
2024-05-28 11:26:28 +0800

    feat: add golang version of 01-hfs-files HW seq

---
+++ golang-hello-world/01-hfs-files/script-hfs-files.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/hashgraph/hedera-sdk-go/v2"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env")
+	}
+
+	accountIdStr := os.Getenv("ACCOUNT_ID")
+	accountKeyStr := os.Getenv("ACCOUNT_PRIVATE_KEY")
+	accountKeyStr = strings.TrimPrefix(accountKeyStr, "0x")
+
+	accountId, err := hedera.AccountIDFromString(accountIdStr)
+	if err != nil {
+		log.Fatalf("Error parsing account ID: %v", err)
+	}
+	accountKey, err := hedera.PrivateKeyFromStringECDSA(accountKeyStr)
+	if err != nil {
+		log.Fatalf("Error parsing account private key: %v", err)
+	}
+	client := hedera.ClientForTestnet()
+	client.SetOperator(accountId, accountKey)
+
+	// Read file from disk
+	localFileContents, err := os.ReadFile("my-file.txt")
+	if err != nil {
+		log.Fatalf("Error reading input file: %v", err)
+	}
+
+	// Write file onto Hedera Testnet, using HFS FileCreateTransaction
+	fileCreateTx, err := hedera.NewFileCreateTransaction().
+		// NOTE: File create transaction
+		// Step (1) in the accompanying tutorial
+		// /* ... */
+		SetContents(localFileContents).
+		FreezeWith(client)
+	if err != nil {
+		log.Fatalf("Error creating FileCreateTransaction: %v", err)
+	}
+	fileCreateTxSigned := fileCreateTx.Sign(accountKey)
+	fileCreateTxSubmitted, err := fileCreateTxSigned.Execute(client)
+	if err != nil {
+		log.Fatalf("Error executing FileCreateTransaction: %v", err)
+	}
+	fileCreateTxId := fileCreateTxSubmitted.TransactionID
+	fileCreateTxReceipt, err := fileCreateTxSubmitted.GetReceipt(client)
+	if err != nil {
+		log.Fatalf("Error getting receipt for FileCreateTransaction: %v", err)
+	}
+	fileId := fileCreateTxReceipt.FileID
+
+	// Read file from Hedera Testnet, using HFS FileContentsQuery
+	fileReadQuery := hedera.NewFileContentsQuery().
+		// NOTE: File contents query
+		// Step (2) in the accompanying tutorial
+		// /* ... */;
+		SetFileID(*fileId)
+	networkFileContents, err := fileReadQuery.Execute(client)
+	if err != nil {
+		log.Fatalf("Error executing for FileContentsQuery: %v", err)
+	}
+
+	txExplorerUrl := fmt.Sprintf("https://hashscan.io/testnet/transaction/%v", fileCreateTxId)
+	fmt.Printf("fileId: %v\n", fileId)
+	fmt.Printf("fileCreateTxId: %v\n", fileCreateTxId)
+	fmt.Printf("txExplorerUrl: %v\n", txExplorerUrl)
+	fmt.Printf("localFileContents: %s\n", localFileContents)
+	fmt.Printf("networkFileContents: %s\n", networkFileContents)
+
+	client.Close()
+}
